8e96e5d6f6bcdb38e72591297593c620d67c2972
simon <peterwakoko@gmail.com>
2022-07-11 11:10:28 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my portfolio</title>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital@1&display=swap" rel="stylesheet"> 
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <!--header section start -->
+    <div class="header" id="top">
+        <nav>
+            <h2 class="logo">Portfo<span>lio</span></h2>
+            <ul>
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="#about">ABOUT US</a></li>
+                <li><a href="#service">SERVICES</a></li>
+                <li><a href="#skill">SKILLS</a></li>
+                <li><a href="#contact">CONTACT US</a></li>
+            </ul>
+            <a href="" class="btn">Subscribe</a>
+        </nav>
+
+    <div class="content">
+    <h4>Hello my name is</h4>
+    <h1>Wakoko <span>Simon Peter</span></h1>
+    <h3>I am a Web Developer.</h3>
+    <div class="newsletter">
+        <form action="">
+            <input type="email" name="email" id="mail" placeholder="Enter Your Email" >
+            <input type="submit" name="submit" value="Let's start">
+        </form>
+    </div>
+    </div>
+
+    </div>
+<!--About section starts-->
+<section class="about" id="about">
+<div class="main">
+<img src="images/back-ground.jpg" alt="" >
+<div class="about-text">
+    <h2>About Me</h2>
+    <h5>Developer <span>and Designer</span></h5>
+    <p>I am full-stack developer. I can provide clean code and pixel perfect  designs.
+        I can maje the website more and more interactive with web animations.A responsive website makes your website accessible to all users, regardless of their device.I can maje the website more and more interactive with web animations.A responsive website makes your website accessible to all users, regardless of their device.
+    </p>
+    <button type="button">Let's  Talk</button>
+</div>
+</div>
+
+</section>
+
+<!-- service section start-->
+<div class="service" id="service">
+
+<div class="title">
+<h2>Our services</h2>
+</div>
+
+<div class="box">
+
+<div class="card">
+<i class="fa-solid fa-bars"></i>
+<h5>Web Development</h5>
+<div class="pra">
+    <p>Every website should be built with two primary goals: Firstly, it needs to work 
+        across all devices. Secondly, it needs to be fast as possible .
+    </p>
+
+    <p style="text-align: center;">
+        <a  class="button" href="#">Read more</a>
+    </p>
+</div>
+</div>
+
+
+<div class="card">
+<i class="fa-solid fa-user"></i>
+<h5>Web Development</h5>
+<div class="pra">
+    <p>Every website should be built with two primary goals: Firstly, it needs to work 
+        across all devices. Secondly, it needs to be fast as possible .
+    </p>
+
+    <p style="text-align: center;">
+        <a  class="button" href="#">Read more</a>
+    </p>
+</div>
+</div>
+
+<div class="card">
+<i class="fa-solid fa-bell"></i>
+<h5>Web Development</h5>
+<div class="pra">
+    <p>Every website should be built with two primary goals: Firstly, it needs to work 
+        across all devices. Secondly, it needs to be fast as possible .
+    </p>
+
+    <p style="text-align: center;">
+        <a  class="button" href="#">Read more</a>
+    </p>
+</div>
+</div>
+
+
+</div>
+
+</div>
+
+<!-- contact me-->
+<div class="contact-me" id="contact">
+    <p>Let Me Get You A Beautiful Website.</p>
+    <a class="button-two" href="#">Hire Me</a>
+</div>
+<!-- Footer start -->
+<footer>
+        <p>Wakoko Simon Peter</p>
+        <p>For more HTML,CSS and coding tutorials- Please click on the 
+            the link below and Subscribe to my channel:
+        </p>
+        <div class="social">
+            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#"><i class="fa-brands fa-dribbble"></i></a>
+        </div>
+        <p class="end">Copyright &copy; By Wakoko Simon Peter</p>
+        <a href="#top" style="color: white;">Back to Top</a>
+    </footer>
+
+</body>
+</html>
+
+
